d2454b7ef798501317af0eb1a761e86d375dad10
Joe Villarba <socagambit@aol.com>
2016-03-12 20:09:20 -0800

    auth moved back

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   'facebook': {
     'clientID': '978197918924359',
     'clientSecret': 'fb945909c61654327e19620a893142a9',
-    'callbackURL': '/auth/facebook/callback',
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
 }
