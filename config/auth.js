6e3d5fbb7cc4e48c57b9936f9cf58265e2bfb427
Joe Villarba <socagambit@aol.com>
2016-03-12 18:00:29 -0800

    updated FB auth

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   'facebook': {
     'clientID': '978197918924359',
     'clientSecret': 'fb945909c61654327e19620a893142a9',
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+    'callbackURL': 'https://evening-depths-97156.herokuapp.com/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
 }
