ec8f239e1e2da698fd74ef845353fa1bcbdfa376
Dan Kaiser <dskaiser82@gmail.com>
2016-03-14 16:09:00 -0700

    API controller the random URL works

--- controllers/api.js
+++ controllers/api.js
@@ -3,39 +3,26 @@ var request = require('request');
 var catList = ["2636","3920","3920_582507_583874", "4044_133012_1045881", "3944",
   "4104", "4171_4191", "5438_426265_1032639","4096","4125_4161","1085632_1229464",
   "2637_667479","4171","4171_1015079","4171_1111647"]
+var mathRan = function (){return (Math.floor(Math.random() * (catList.length - 0)) + 0)}
 
 
 module.exports = {
-  catList: catList,
-  mathRan: Math.floor(Math.random() * (catList.length - 0)) + 0,
+
 //show api
-  show1: function(req,res){ //for home page
-    var url = "http://api.walmartlabs.com/v1/paginated/items?format=json&category="+ catList[1]+"&apiKey=frt6ajvkqm4aexwjksrukrey"
+  show: function(req,res){ //for home page
+    var url = "http://api.walmartlabs.com/v1/paginated/items?format=json&category="+ catList[mathRan()]+"&apiKey=frt6ajvkqm4aexwjksrukrey"
 
     request({url: url, json: true}, function(error, response, body){
       console.log(body.items[0])
       var data = { //this for our index view, myHeader is a variable for the home page view
-        imageResult: body.items[0]
+        imageResult: body.items
       }
 
       res.json(data)
     })
-
-  },
-
-  show2: function(req,res){ //for home pag
-
-
-    request({url: url, json: true}, function(error, response, body){
-      console.log(body.items[0])
-      var data = { //this for our index view, myHeader is a variable for the home page view
-        imageResult: body.items[0]
-      }
-
-      res.json(data)
-    })
-
 
   }
+
+
 
  }
--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,8 @@ var
 
 
 //api route for product JSON
-apiRouter.get('/api1', apiCtrl.show1)
-apiRouter.get('/api2', apiCtrl.show2)
-// apiRouter.get('/api3', apiCtrl.show3)
-// apiRouter.get('/api4', apiCtrl.show4)
+apiRouter.get('/api', apiCtrl.show)
+
 
 
 module.exports = apiRouter
