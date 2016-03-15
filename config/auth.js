d265c2eece5b34fe7a000dc09c99a8ab6e55cedb
Joe V <socagambit@aol.com>
2016-03-15 16:44:01 -0700

    remove config/

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'facebook': {
-    'clientID': '978197918924359',
-    'clientSecret': 'fb945909c61654327e19620a893142a9',
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
-    'profileFields': ['emails', 'displayName']
-  }
-}
